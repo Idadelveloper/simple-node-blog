1052e5adf8e02799b5b133b0f95825f44f5085bd
Ida Delphine <idadelm@gmail.com>
2022-04-24 15:22:50 +0100

    Added view templates

--- app.js
+++ app.js
@@ -5,20 +5,31 @@ const app = express();
 // register view engine
 app.set('view engine', 'ejs');
 
+// listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', { root: __dirname });
+    const blogs = [
+        { title: "Getting started in open source", snippet: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
+        { title: "Stept to becoming a Full Stack Developer", snippet: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
+        { title: "My UNICEF internship experience", snippet: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." }
+    ]
+
+    res.render('index', { title: "Home" });
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', { title: "About" });
 });
 
 app.get('/about-us', (req, res) => {
     res.redirect('/about');
 })
 
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: "Create Blog" });
+})
+
 app.use((req, res) => {
-    res.sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404', { title: "Not Found" });
 })
