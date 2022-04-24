ec2a5f7ade20e39a11ec8235ad0140e8b44e56c5
Ida Delphine <idadelm@gmail.com>
2022-04-24 19:53:25 +0100

    Enabled creation of blog post

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.set('view engine', 'ejs');
 
 // middleware and static files
 app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'));
 
 // app routes
@@ -41,6 +42,18 @@ app.get('/blogs', (req, res) => {
         })
         .catch((err) => {
             console.log(err);
+        })
+})
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs')
+        })
+        .catch((err) => {
+            console.log(err)
         })
 })
 
